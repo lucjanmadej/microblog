2fe79adaf9827946ab2e9a4ffa39cb08b4f9d7f9
lucjanmadej <l.madej@outlook.com>
2022-05-16 14:12:27 +0200

    Add more mockup posts for testing

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,18 @@ def index():
         },
         {   'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Randy'},
+            'body': 'I badly want a cheeseburger'
+        },
+        {
+            'author': {'username': 'Sheriff'},
+            'body': 'Bla bli bla'
+        },
+        {
+            'author': {'username': 'Mike'},
+            'body': 'Da di doo'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
