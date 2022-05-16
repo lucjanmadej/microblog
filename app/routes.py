f1c8467c0a1f87e815bde46515794a2cadfd3344
lucjanmadej <l.madej@outlook.com>
2022-05-16 13:51:32 +0200

    Add some mockup posts

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,13 @@ from app import app
 @app.route('/index')
 def index():
     user = {"username": "Lucjan"}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful Day in Portland!'
+        },
+        {'author': {'username': 'Susan'},
+         'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html', title='Home', user=user)
